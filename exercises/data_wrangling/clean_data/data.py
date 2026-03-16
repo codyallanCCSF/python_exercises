6ad52db80949cb6b8a2e2e9b8edf3d6f8407100b
codyallanCCSF <cgilber2@mail.ccsf.edu>
2026-03-15 22:06:36 -0700

    01 - Adds list of raw data

---
+++ exercises/data_wrangling/clean_data/data.py
@@ -0,0 +1,9 @@
+# Messy strings that need parsing
+
+raw_logs = [
+    "USER:codyallan | ACTION:login | TIMESTAMP:2026-03-15T19:30:00Z",
+    "USER:bob_mgr | ACTION:update_inv | SKU:KB-99 | QTY:+10",
+    "ERROR: SFO-03 is currently OFFLINE. Attempting reconnect...",
+    "SUCCESS: Sale completed SKU:MO-42 Price:55.50",
+    "SYSTEM_ALERT: low_stock SKU:MN-10 Zone:A"
+]
