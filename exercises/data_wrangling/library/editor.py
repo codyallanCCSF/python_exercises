d57a5c8ade08a6384b1b08442da43519ec96dca9
codyallanCCSF <cgilber2@mail.ccsf.edu>
2026-03-16 20:36:21 -0700

    02 - Adds methods to link Manager class and test the link

---
+++ exercises/data_wrangling/library/editor.py
@@ -0,0 +1,11 @@
+# Editor class declaration
+
+class Editor:
+
+    # Links Manager to Editor
+    def link_manager(self, manager_instance):
+        self.mgr = manager_instance
+
+    # Method to test link
+    def verify_editor(self):
+        return f"Editor Handshake: {self.mgr.system_status}"
