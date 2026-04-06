8404e67b7487ce8fcd98bccd8549c88b3d360f6a
codyallanCCSF <codybizniz@proton.me>
2026-04-05 18:13:42 -0700

    01 - Adds main function signature

---
+++ exercises/game_dev/number_guesser/main.py
@@ -0,0 +1,16 @@
+"""
+File: main.py
+Author: Cody Gilbert
+Date: 04/05/2026
+
+Description:
+    Main progam - Controls implements game loop and
+        user interactions.
+"""
+
+def main():
+    game = Guesser(1, 100)
+
+
+if __name__ == "__main__":
+    main()
